d07454131d274929c137a308035c6db9b29725cb
Atralupus <me@atralupus.com>
2023-08-01 02:32:42 +0900

    sample my_add

---
+++ DotnetTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CsBindgen
+{
+    class Program {
+        static void Main(string[] args) {
+            var r = NativeMethods.my_add(1, 2);
+            Console.WriteLine(r);
+        }
+    }
+}
