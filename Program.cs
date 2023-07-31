a5631bbd17bb1c970d914c64b6aa413cc84f669f
Atralupus <me@atralupus.com>
2023-08-01 01:49:34 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program {
+    [DllImport("libbencodex.dylib")]
+    public static extern int Encode(int a);
+
+    static void Main(string[] args) {
+        var testvalue = Encode(1);
+        Console.WriteLine(testvalue);
+    }
+}
